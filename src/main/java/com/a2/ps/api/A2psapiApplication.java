ee87ddb6f34a03b7662b7eafc3ebd4e596455cf1
paulocarvalho <paulocarvalho@mppa.mp.br>
2025-04-24 10:40:48 -0300

    primeiro commit

---
+++ src/main/java/com/a2/ps/api/A2psapiApplication.java
@@ -0,0 +1,13 @@
+package com.a2.ps.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2psapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2psapiApplication.class, args);
+    }
+
+}
